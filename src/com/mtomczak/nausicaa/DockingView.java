e7967b4890407416baf4a52495aabbff09374929
Mark Tomczak <fixermark@gmail.com>
2014-09-17 23:23:02 -0400

    Added coordinate set for pitch and yaw indicators.

--- src/com/mtomczak/nausicaa/DockingView.java
+++ src/com/mtomczak/nausicaa/DockingView.java
@@ -16,6 +16,55 @@ import org.json.JSONException;
  * A view useful for aligning ships for docking.
  */
 public class DockingView extends RelativeLayout implements TelemetryViewer {
+  private static final float[] PITCH_IMAGE = {
+    70, 232.5,
+    83, 226.5,
+    96, 213.5,
+    101, 200.5,
+    281, 200.5,
+    315, 152.5,
+    330, 152.5,
+    315, 198.5,
+    307, 207.5,
+    324, 204.5,
+    324, 225.5,
+    306, 220.5,
+    314, 228.5,
+    227, 228.5,
+    242, 247.5,
+    203, 247.5,
+    170, 228.5,
+    70, 234.5
+    };
+
+  private static final float[] YAW_IMAGE = {
+    200, 102,
+    210, 102,
+    216, 118,
+    216, 205,
+    219, 213,
+    265, 259,
+    268, 268,
+    267, 277,
+    214, 277,
+    221, 298,
+    203, 298,
+    205, 273,
+    195, 273,
+    197, 298,
+    179, 298,
+    186, 277,
+    133, 277,
+    132, 268,
+    135, 259,
+    181, 213,
+    184, 205,
+    184, 118,
+    190, 102,
+    200, 102,
+  };
+
+  // TODO(mtomczak): Allow dials to take in Path objects to render, and bake the paths here.
   private AlertView alertOutput = null;
   private Dial horizontalDial = null;
   private Dial verticalDial = null;
